2af85c38ea618ab3c03f7094936776b4ecf3839f
Stephan Telling <st@telling.xyz>
2016-06-05 17:30:07 +0200

    Improvements

--- Geokoderen/geokoderen.py
+++ Geokoderen/geokoderen.py
@@ -20,18 +20,27 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon, QMessageBox
-from qgis.core import QgsCoordinateReferenceSystem
-from qgis.core import QgsCoordinateTransform
-from pointtool import PointTool
-# Initialize Qt resources from file resources.py
-import resources
 import os.path
 import webbrowser
+from PyQt4.QtCore import (
+    QSettings,
+    QTranslator,
+    qVersion,
+    QCoreApplication
+)
+from PyQt4.QtGui import (
+    QAction,
+    QIcon,
+    QMessageBox
+)
+from qgis.core import QgsCoordinateReferenceSystem
+from qgis.core import QgsCoordinateTransform
+from .pointtool import PointTool
+# Initialize Qt resources from file resources.py
+import resources
 
 
-class Geokoderen:
+class Geokoderen(object):
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
--- Geokoderen/pointtool.py
+++ Geokoderen/pointtool.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+        begin                : 2015-10-22
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Septima P/S
+        email                : kontakt@septima.dk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 from qgis.gui import QgsMapTool
 from PyQt4.QtCore import pyqtSignal
 
